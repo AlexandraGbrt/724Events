b91389d69cd5a15f1e1339f7e1bfd5e99f34c003
Alexandra Gaubert <alexandra.gaubert95@gmail.com>
2025-05-13 11:34:18 +0200

    Dernière vérif ok

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -28,28 +28,4 @@ describe("When Events is created", () => {
   });
 });
 
-// describe("and a click is triggered on the submit button", () => {
-//   it("displays a confirmation message on successful submission", async () => {
-//     const onSuccess = jest.fn();
-//     const onError = jest.fn();
-//     render(<Form onSuccess={onSuccess} onError={onError} />);
 
-//     // Simulez le remplissage du formulaire
-//     fireEvent.change(screen.getByLabelText(/Nom/i), { target: { value: "Doe" } });
-//     fireEvent.change(screen.getByLabelText(/Prénom/i), { target: { value: "John" } });
-//     fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: "test@example.com" } });
-
-//     // Simulez le clic sur le bouton
-//     fireEvent.click(await screen.findByTestId("button-test-id"));
-
-//     // Attendez que le texte "En cours" soit affiché (optionnel)
-//     await screen.findByText("En cours");
-
-//     // Vérifiez que la fonction onSuccess a bien été appelée
-//     expect(onSuccess).toHaveBeenCalled();
-
-//     // Ajoutez une vérification pour le message de confirmation
-//     const confirmationMessage = await screen.findByText(/Votre message a été envoyé avec succès!/i); // Ajustez le texte selon ce que vous avez défini
-//     expect(confirmationMessage).toBeInTheDocument();
-//   });
-// });
